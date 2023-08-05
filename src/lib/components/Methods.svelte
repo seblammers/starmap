feb801e79827c8c58b0df6394f6f7acc973c9c77
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-08-05 22:18:06 +0200

    init methods

---
+++ src/lib/components/Methods.svelte
@@ -0,0 +1,29 @@
+<h2>Methodology</h2>
+
+<p>Data were collected and merged from 3 sources:</p>
+
+<ol>
+  <li>
+    <a href="https://github.com/ofrohn/d3-celestial" target="_blank"
+      >d3 celestial</a
+    >
+  </li>
+  <li>
+    <a href="https://github.com/astronexus/HYG-Database" target="_blank"
+      >HYG Database</a
+    >
+  </li>
+  <li>
+    <a
+      href="https://en.wikipedia.org/wiki/Timelines_of_world_history"
+      target="_blank">Wikipedia</a
+    >
+  </li>
+</ol>
+
+<p>
+  For more details, feel free to check out the code for <a
+    href="https://github.com/seblammers/starmap"
+    target="_blank">this project on Github</a
+  >.
+</p>
