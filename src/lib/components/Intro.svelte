9180d01ce3e40aa1bc6cdce5f968c610cad4f3b6
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-08-06 20:23:17 +0200

    tweak intro

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -31,46 +31,54 @@
 <div class="spacer" />
 
 <p>
-  Hi Philip, ich hab jetzt mal hier wild reingeschrieben. Werde das
-  wahrscheinlich noch ausdünnen oder abändern. :)
+  We have been gazing at the stars since the dawn of humankind. They helped us
+  navigate the oceans, understand the cyclic nature of the seasons and inspired
+  a myriad of stories.
 </p>
-<p>So, the Sun is quite far away, right?</p>
-<p>It's actually 149,600,000 km.</p>
+
+<p>This project is not about all that.</p>
+
 <p>
+  It's about a tiny aspect that is often overlooked. The stars our ancestors
+  were looking at, we are looking at, and future generations will be looking at,
+  they seem to be so reliable. They just keep on sending their light towards us,
+  so we can marvel at them.
+</p>
+
+<p>
+  But the crazy distances that the light needs to travel in order to reach your
+  eyes...? That is <em>astonishing</em>.
+</p>
+
+<p>
+  You might think the Sun is quite far away, right? Well, it's 149,600,000 km.
   For perspective: if you travel from Paris to New York and then proceed to fly
   across the US all the way to Hong Kong and then onwards back to Paris, you
   traveled merely 28,081 km. Which means it would take more than 5,000 of those
-  trips to go to the sun.
+  trips to go to the Sun. Turns out planes are not so fast after all.
 </p>
 <p>
-  Turns out planes are not so fast after all. But you know what is super fast?
-  Light. Yes.
-</p>
-<p>It takes the light just 8 min 19 s to go from the Sun to your eyes!</p>
-
-<p>
-  So it makes sense that we measure the vast distances of stars and other
-  celestial bodies in light years.
+  But you know what is super fast? Light. Yes. It takes the light just 8 min 19
+  s to go from the Sun to your eyes! So it makes sense that we measure the vast
+  distances of stars and other celestial bodies in light years.
 </p>
 
 <p>
   What do you think how many kms can light travel in one year? How much is a
-  light year?
+  light year? 9.46 trillion kilometers. Yes, that's right: 9,460,000,000,000 km.
 </p>
-
-<p>9.46 trillion kilometers. Yes, that's right: 9,460,000,000,000 km.</p>
 
 <p>I know, these numbers are hard to grasp.</p>
 
 <p>
   Let's make that more clear: Here is how long the light needs to travel from
-  the sun to earth if one light year would take one second:
+  the Sun to you <em>if one light year would take one second</em>:
 </p>
 
 <DotPlot data={dataSun} />
 
 <p>
-  Compare that to one of the brightest Stars <em>Sirius</em>, which 8.6 light
+  Compare that to one of the brightest Stars <em>Sirius</em>, which is 8.6 light
   years away:
 </p>
 
@@ -83,8 +91,8 @@
 
 <p>
   But what fascinates me even more is to think about the time on earth that
-  connects me, seeing the stars with the people on Earth that existed when the
-  star light started traveling towards me.
+  connects us, seeing the stars with the people on Earth that existed when the
+  star light started traveling towards us.
 </p>
 
 <p>
@@ -104,5 +112,6 @@
   the time when the light started traveling towards you. Try to zoom in to be
   able to better select which star you learn about. It's easy to get lost in
   there, so if you want to go back to the initial overview just hit the "Reset"
-  button.
+  button. All events were taken from Wikipedia timelines and roughly matched to
+  the respective amount of light years.
 </p>
