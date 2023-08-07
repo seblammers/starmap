2642fa0373e522043f2907a6844d0364ecfcb65f
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-08-07 21:11:06 +0200

    remove bouncing arrow

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -10,7 +10,6 @@
       >
     </div>
   </div>
-  <div class="scroll" />
 </div>
 
 <style>
@@ -53,31 +52,5 @@
   .byline {
     font-variant-caps: small-caps;
     font-size: var(--step-1);
-  }
-  .scroll {
-    position: relative;
-    width: 100%;
-  }
-
-  .scroll::before {
-    animation: bounce 1s ease infinite;
-
-    color: var(--accent);
-    content: "↓";
-    font-size: var(--step-4);
-
-    left: 50%;
-
-    position: absolute;
-    text-align: center;
-    text-shadow: 0 0 20px var(--light), 0 0 30px var(--dark),
-      0 0 40px var(--dark), 0 0 50px var(--dark), 0 0 60px var(--dark),
-      0 0 70px var(--dark), 0 0 80px var(--dark);
-  }
-
-  @keyframes bounce {
-    50% {
-      transform: translateY(-10%);
-    }
   }
 </style>
