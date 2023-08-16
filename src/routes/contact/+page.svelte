fd950632d251a28ef32e2200c4e30d04107819bb
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-08-16 22:38:29 +0200

    init contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Contact from "$lib/components/Contact.svelte";
+</script>
+
+<svelte:head>
+  <title>Stories from the night sky | Contact</title>
+</svelte:head>
+
+<h2>Contact</h2>
+
+<article class="flow">
+  <section>
+    <p>Do have feedback? Praise? Errors? Bugs? Questions?</p>
+    <p>Let me know!</p>
+  </section>
+
+  <Contact />
+</article>
---
+++ src/routes/contact/success/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Stories from the night sky | Contact</title>
+</svelte:head>
+
+<h2>Thank you!</h2>
+
+<p>Your message was sent to my inbox.</p>
